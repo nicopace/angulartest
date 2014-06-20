50ec639705ab867444e01dacc29fb652d897132a
Nicolas Pace <nicolas.pace@unixono.com.ar>
2014-06-20 18:04:12 -0300

    Remove unused dependencies.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,12 +10,8 @@
  */
 angular
   .module('angulartestApp', [
-    'ngAnimate',
-    'ngCookies',
     'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
+    'ngRoute'
   ])
   .config(function ($routeProvider) {
     $routeProvider
