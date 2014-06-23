126909138b9b0834f60e995263bfc62391b85ec8
Nicolas Pace <nicolas.pace@unixono.com.ar>
2014-06-22 21:27:14 -0300

    Add Angular-Bootstrap dependency.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,24 @@
  * Controller of the angulartestApp
  */
 angular.module('angulartestApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+
+    $scope.selected = undefined;
+
+    $scope.getActors = function(val) {
+      return $http.get('http://api.themoviedb.org/3/search/person', {
+        params: {
+          'query': val,
+          'search_type': 'ngram',
+          'api_key': 'cecc69d5a147e6ef0ac176b35d44f9e3'
+        }
+      }).then(function(res){
+        var addresses = [];
+        angular.forEach(res.data.results, function(item){
+          addresses.push(item.name);
+        });
+        return addresses;
+      });
+    };
+
   });
